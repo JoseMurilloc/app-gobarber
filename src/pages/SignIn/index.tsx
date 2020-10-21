9d410494b9e23c8d3caf8b88e838a0af80cef84c
Jose Murillo <jooseemurillo@gmail.com>
2020-10-21 09:19:54 -0300

    Create component Input and Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
+import logoImage from '../../assets/logo.png'
+
 import { Image, Text } from 'react-native';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Container, Title } from './styles';
 
-import logoImage from '../../assets/logo.png'
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImage} />
       <Title>Faça seu login</Title>
+
+      <Input
+        name="email"
+        icon="mail"
+        placeholder="E-mail"
+      />
+      <Input
+        name="password"
+        icon="lock"
+        placeholder="Senha"
+      />
+
+      <Button onPress={() => console.log("Ok")}>
+        Entrar
+      </Button>
     </Container>
   );
 };
