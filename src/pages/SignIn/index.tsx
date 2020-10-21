b1b72341f8dd9c00a2ebc8356b3aab559774e7e0
Jose Murillo <jooseemurillo@gmail.com>
2020-10-21 07:53:02 -0300

    Import fonts on project

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image, Text } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 import logoImage from '../../assets/logo.png'
 
@@ -9,6 +9,7 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImage} />
+      <Title>Faça seu login</Title>
     </Container>
   );
 };
